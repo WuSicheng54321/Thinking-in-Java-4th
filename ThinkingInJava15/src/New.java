251f5efe49d5776a0358d1475e6e2603c8f6fd52
wusicheng <1656423700@qq.com>
2016-02-04 20:53:38 +0800

    Thinking in Java 4th

---
+++ ThinkingInJava15/src/New.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class New {
+	public static<K,V> Map<K,V> map(){
+		return new HashMap<K,V>();
+	}
+	public static <T> List<T> list(){
+		return new ArrayList<T>();
+	}
+	public static<T> LinkedList<T> lList(){
+		return new LinkedList<T>();
+	}
+	public static <T>  Set<T> set(){
+		return new HashSet<T>();
+	}
+	public static <T> Queue<T> queue(){
+		return new LinkedList<T>();
+	}
+	public static void main(String args[]){
+		Map<String,List<String>> sls=New.map();
+		List<String> lis=New.list();
+		LinkedList<String> lls=New.lList();
+		Set<String> ss=New.set();
+		Queue<String> qs=New.queue();
+	}
+}
---
+++ ThinkingInJava15/src/Test11.java
@@ -0,0 +1,5 @@
+import java.util.Map;
+
+public class Test11 {
+	Map<String,Integer> ls=New.map();
+}
